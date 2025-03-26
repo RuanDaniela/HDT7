9886481513dc8d49dcbcd394431bb3ad001b7a58
SofiBatz14 <sofibatz14@gmail.com>
2025-03-25 22:55:24 -0600

    cambios en pruebas unitarias de 2 clases

--- hdt7/src/test/java/com/example/BinarySearchTreeTest.java
+++ hdt7/src/test/java/com/example/BinarySearchTreeTest.java
@@ -1,9 +1,11 @@
 package com.example;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.platform.commons.annotation.Testable;
 
 class BinarySearchTreeTest
 {
